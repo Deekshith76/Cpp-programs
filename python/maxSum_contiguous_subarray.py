2107c48f163eeaece3b237f63b6d1ce29e9b1fef
Deekshith76 <deekshith_b180791ce@nitc.ac.in>
2021-08-19 21:09:13 +0530

    Added some other methods

--- python/maxSum_contiguous_subarray.py
+++ python/maxSum_contiguous_subarray.py
@@ -9,5 +9,31 @@ def solve(nums):
         
     return max_so_far
 
+def maxSubarray3(nums):
+    # TC : O(n^2)
+    # SC : O(1)
+    max_sum = -100000
+    cur_sum = 0
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            cur_sum += nums[j]
+            max_sum = max(cur_sum, max_sum)
+        cur_sum = 0
+    return max_sum
+
+def maxSubarray2(nums):
+    # TC : O(n^3)
+    # SC: O(1)
+    max_sum = -1000000
+    cur_sum = 0
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            for k in range(i, j+1):
+                cur_sum += nums[k]
+            max_sum = max(cur_sum, max_sum)
+            cur_sum = 0
+            
+    return max_sum
+
 A = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 print(solve(A))
