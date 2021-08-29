3df45a162a6e1e68800f90c83c971c58d60f8a47
Deekshith76 <deekshith_b180791ce@nitc.ac.in>
2021-08-29 20:40:03 +0530

    Balanced Binary Tree Check (Easy) LC.110

---
+++ python/balanced_binary_tree.py
@@ -0,0 +1,52 @@
+'''
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+Example 3:
+
+Input: root = []
+Output: true
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-104 <= Node.val <= 104
+'''
+
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isBalanced(self, root): #returns bool
+        
+        def dfs(root): #returns [bool, height]
+            if not root: return [True, 0]
+            
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+            
+            return [balanced, 1 + max(left[0], right[0])]
+        
+        return dfs(root)[0]
+    
+# Bottom Up recursion
+# Time complexity: O(n) where n is number of nodes as we are visiting each node exactly once
